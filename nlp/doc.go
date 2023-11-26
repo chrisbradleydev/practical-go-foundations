89be4d88e0e3801e41f0f1ecef5b09be38124680
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2023-11-26 00:43:24 -0500

    day 04 - part 01

---
+++ nlp/doc.go
@@ -0,0 +1,9 @@
+/*
+Package nlp provides natural language processing utilities
+
+To learn more about Doc Comments, see the article
+[Go Doc Comments].
+
+[Go Doc Comments]: https://tip.golang.org/doc/comment
+*/
+package nlp
---
+++ nlp/nlp_test.go
@@ -0,0 +1,20 @@
+package nlp
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTokenize(t *testing.T) {
+	text := "What's on second?"
+	expected := []string{"what", "s", "on", "second"}
+	tokens := Tokenize(text)
+	require.Equal(t, expected, tokens)
+	/* Before testify
+	// if tokens != expected {} // can't compare slices with == in Go (only to nil)
+	if !reflect.DeepEqual(expected, tokens) {
+		t.Fatalf("expected %#v, got %#v", expected, tokens)
+	}
+	*/
+}
